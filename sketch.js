38c0b4daab49251d6c604cbe5dd5e0f00399fb39
sirrah23 <hseth93@gmail.com>
2017-02-15 00:44:10 -0500

    Tweaked movement and boundary logic

--- sketch.js
+++ sketch.js
@@ -4,25 +4,33 @@ var net;
 
 function setup(){
   createCanvas(1200, 500);
-  p1 = new Slime(200, height, createVector(255, 0, 0), "LEFT");
-  p2 = new Slime(width-200, height, createVector(0, 255, 0), "RIGHT");
   net = new Net();
+  p1 = new Slime(200, height, createVector(255, 0, 0), "LEFT", net);
+  p2 = new Slime(width-200, height, createVector(0, 255, 0), "RIGHT", net);
 }
 
 function draw(){
   background(59, 119, 214);
   if (keyIsDown(65)){
-    p1.moveLeft(net);
+    p1.run(-8);
   }
   if (keyIsDown(68)){
-    p1.moveRight(net);
+    p1.run(8);
+  }
+  if (keyIsDown(87)){
+    p1.jump();
   }
   if (keyIsDown(LEFT_ARROW)){
-    p2.moveLeft(net);
+    p2.run(-8);
   }
   if (keyIsDown(RIGHT_ARROW)){
-    p2.moveRight(net);
+    p2.run(8);
   }
+  if (keyIsDown(UP_ARROW)){
+    p2.jump();
+  }
+  p1.move();
+  p2.move();
   p1.show();
   p2.show();
   net.show();
--- slime.js
+++ slime.js
@@ -1,10 +1,12 @@
-function Slime(x, y, color, side){
+function Slime(x, y, color, side, net){
   this.x = x;
   this.y = y;
-  this.side = side;
-  this.speed = 8;
-  this.diameter = 100;
   this.color = color;
+  this.side = side; // Side of the net the slime is on
+  this.net = net;   // The in-game net object
+  this.xspeed = 0;
+  this.yspeed = 0;
+  this.diameter = 100;
 
   this.show = function(){
     push();
@@ -13,29 +15,62 @@ function Slime(x, y, color, side){
     pop();
   }
 
-  this.moveLeft = function(net){
-    this.x -= this.speed;
+  // Check left side of slime for collisions
+  this.checkLeft = function(){
     if (this.side == "LEFT"){
       if (this.x - this.diameter/2 < 0){
         this.x = this.diameter/2;
       }
     } else {
-      if ((this.x - this.diameter/2) < (net.x + net.width)){
-        this.x = net.x + this.diameter/2;
+      if ((this.x - (this.diameter/2)) < (this.net.x)){
+        this.x = this.net.x + this.diameter/2;
       }
     }
   }
 
-  this.moveRight = function(net){
-    this.x += this.speed;
+  // Check right side of slime for collisions
+  this.checkRight = function(){
     if (this.side == "LEFT"){
-      if ((this.x + this.diameter/2) > (net.x)){
-        this.x = net.x - this.diameter/2;
+      if ((this.x + this.diameter/2) > (this.net.x)){
+        this.x = this.net.x - this.diameter/2;
       }
     } else{
       if (this.x + this.diameter/2 > width){
         this.x = width-this.diameter/2;
       }
     }
+  }
+
+  this.checkBoundary = function(){
+    this.checkLeft();
+    this.checkRight();
+  }
+
+  this.stopRun = function(){
+    this.xspeed = 0;
+  }
+
+  this.run = function(speed){
+    this.xspeed = speed;
+  }
+
+  this.jump = function(){
+    this.yspeed = 20;
+  }
+
+  this.applyGravity = function(){
+    if (this.y < height)
+      this.yspeed -=5;
+    else{
+      // TODO: Move to check boundary?
+      this.y = height;
+    }
+  }
+
+  this.move = function(){
+    this.x += this.xspeed;
+    this.y -= this.yspeed;
+    this.applyGravity();
+    this.checkBoundary();
   }
 }
